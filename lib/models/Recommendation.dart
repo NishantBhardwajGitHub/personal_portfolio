566ccced174564489b64af5ff7e58bc485ff2d38
Asus <nishantbhardwaj2367@gmail.com>
2023-04-29 11:28:15 +0530

    second commit

--- lib/models/Recommendation.dart
+++ lib/models/Recommendation.dart
@@ -6,7 +6,7 @@ class Recommendation {
 
 final List<Recommendation> demo_recommendations = [
   Recommendation(
-    name: "Kolapo Obanewa",
+    name: "Compiler",
     source: "Linkedin",
     text:
         "I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about his work is his ability to write clean and reusable codes with best practices. He is hardworking and makes it so easy to transcribe ideas into beautiful and testable flutter apps. He is a Flutter/Dart gem and has my highest recommendation",
